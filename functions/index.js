5ee477b88807180c7fd8439d29eef18b32b7a70c
Dylan <dachaconp@unal.edu.co>
2020-11-10 11:35:35 -0500

    delete stories

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
+/* eslint-disable promise/no-nesting */
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.deleteStories24Hours = functions.pubsub.schedule("every 12 hours").onRun(async(context)=>{
+    const now = Date.now();
+    const oneDay = 24 * 60 * 60 * 1000;
+    const yesterday = new Date(now - oneDay);
+    await admin.firestore().collection('/stories').where('createdAt', '<', yesterday)
+    .get().then( (snapshot)=> {
+        snapshot.forEach((doc)=> {
+             doc.ref.delete()
+             .then((res) => console.log(res))
+             .catch((err) => console.log(err))
+        });
+        return ;
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+});
